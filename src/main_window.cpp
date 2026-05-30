e92d53718b54922889bb658cdf9aa1ee4390872a
wang_y <3084003139@qq.com>
2026-05-30 22:35:10 +0800

    提高mian_window响应速度

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -10,10 +10,12 @@ main_window::main_window(QWidget *parent)
     , ui(new Ui::main_window)
 {
     ui->setupUi(this);
+    widget = new Widget(this);
+    widget->setWindowFlags(Qt::Window);
+    this->setAttribute(Qt::WA_DeleteOnClose);
     connect(ui->CreateMeeting_button, &QPushButton::clicked, this, &main_window::CreateMeeting_button_clicked);
     connect(ui->JoinMeeting_button, &QPushButton::clicked, this, &main_window::JoinMeeting_button_clicked);
     connect(ui->ConnectToServer_button, &QPushButton::clicked, this, &main_window::ConnectToServer_button_clicked);
-
 }
 
 main_window::~main_window()
@@ -24,16 +26,16 @@ main_window::~main_window()
 
 void main_window::CreateMeeting_button_clicked() {
     LOG_INFO("main_window", "点击创建会议按钮");
-    if(widget != nullptr)  {
+    if(widget->isVisible())  {
         QMessageBox::warning(this, "warning", "目前有一打开的会议");
         return;
     }
-    widget = new Widget(nullptr);
-    connect(widget,&QObject::destroyed,this,[this](){
-        widget = nullptr;
-    });
+    //widget = new Widget(this);
+    // connect(widget,&QObject::destroyed,this,[this](){
+    //     widget = nullptr;
+    // });
     //widget->setWindowFlags(Qt::Window);
-    widget->setAttribute(Qt::WA_DeleteOnClose);
+    //widget->setAttribute(Qt::WA_DeleteOnClose);
     widget->show();
     widget->on_connServer(this->ip, this->port);
     widget->on_createmeetBtn_clicked();
